d6604cd576a86f3693eb92623360698863923a56
Hannan Khan <hannankhan888@gmail.com>
2021-12-17 01:51:32 -0600

    updated open chrome code

--- utils/actions.py
+++ utils/actions.py
@@ -1,12 +1,16 @@
 import webbrowser
 import winapps
 
+# key is the application name (str), value is the rest of the info.
+app_results = {}
 for item in winapps.search_installed('chrome'):
-    print(item)
-
+    app_results[item.name] = item
 
 url1 = 'youtube.com'
 
-chrome_path = r'C:\Program Files\Google\Chrome\Application\chrome.exe'
+for key, value in app_results.items():
+    print(key, "===>", value)
+
+chrome_path = app_results['Google Chrome'].install_location
 webbrowser.register('chrome',None, webbrowser.BackgroundBrowser(chrome_path))
 webbrowser.get('chrome').open_new_tab(url1)
