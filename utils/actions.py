a28fd424634602a0e596fa916b0ec119bfdb3766
Ali Abdul-Hameed <ali1.abdulhameed@gmail.com>
2021-12-17 01:57:43 -0600

    Fixed chrome path. Now anyone who uses this program can open chrom from the application.

--- utils/actions.py
+++ utils/actions.py
@@ -11,6 +11,7 @@ url1 = 'youtube.com'
 for key, value in app_results.items():
     print(key, "===>", value)
 
-chrome_path = app_results['Google Chrome'].install_location
-webbrowser.register('chrome',None, webbrowser.BackgroundBrowser(chrome_path))
+chrome_path = app_results['Google Chrome'].install_location.joinpath('chrome.exe')
+print(chrome_path)
+webbrowser.register('chrome', None, webbrowser.BackgroundBrowser(str(chrome_path)))
 webbrowser.get('chrome').open_new_tab(url1)
