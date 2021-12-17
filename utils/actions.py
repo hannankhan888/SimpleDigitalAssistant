005c70b7b748df97545537728748a46e9869b262
Ali Abdul-Hameed <ali1.abdulhameed@gmail.com>
2021-12-17 02:19:19 -0600

    Fixed chrome path again. Now anyone who uses this program can open chrome from the application.

--- utils/actions.py
+++ utils/actions.py
@@ -1,17 +1,19 @@
+import winreg
+import re
 import webbrowser
-import winapps
-
 # key is the application name (str), value is the rest of the info.
 app_results = {}
-for item in winapps.search_installed('chrome'):
-    app_results[item.name] = item
+
 
 url1 = 'youtube.com'
 
 for key, value in app_results.items():
     print(key, "===>", value)
 
-chrome_path = app_results['Google Chrome'].install_location.joinpath('chrome.exe')
+
+command = winreg.QueryValueEx(winreg.OpenKey(winreg.HKEY_CLASSES_ROOT, "ChromeHTML\\shell\open\\command", 0, winreg.KEY_READ), "")[0]
+chrome_path=re.search("\"(.*?)\"", command).group(1)
+
 print(chrome_path)
 webbrowser.register('chrome', None, webbrowser.BackgroundBrowser(str(chrome_path)))
 webbrowser.get('chrome').open_new_tab(url1)
