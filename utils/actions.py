24057fc23a9642f48767de84447735ba2b7fed75
salma <salman.nazir@mavs.uta.edu>
2021-12-17 02:50:35 -0600

    Lowers your volume by half after running application

--- utils/actions.py
+++ utils/actions.py
@@ -1,15 +1,15 @@
 import winreg
 import re
 import webbrowser
+from ctypes import cast, POINTER
+from comtypes import CLSCTX_ALL
+from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
 # key is the application name (str), value is the rest of the info.
 app_results = {}
-
-
 url1 = 'youtube.com'
 
 for key, value in app_results.items():
     print(key, "===>", value)
-
 
 command = winreg.QueryValueEx(winreg.OpenKey(winreg.HKEY_CLASSES_ROOT, "ChromeHTML\\shell\open\\command", 0, winreg.KEY_READ), "")[0]
 chrome_path=re.search("\"(.*?)\"", command).group(1)
@@ -17,3 +17,14 @@ chrome_path=re.search("\"(.*?)\"", command).group(1)
 print(chrome_path)
 webbrowser.register('chrome', None, webbrowser.BackgroundBrowser(str(chrome_path)))
 webbrowser.get('chrome').open_new_tab(url1)
+
+import math
+# Get default audio device using PyCAW
+devices = AudioUtilities.GetSpeakers()
+interface = devices.Activate(
+    IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
+volume = cast(interface, POINTER(IAudioEndpointVolume))
+# Get current volume
+currentVolumeDb = volume.GetMasterVolumeLevel()
+volume.SetMasterVolumeLevel(currentVolumeDb - 6.0, None)
+# NOTE: -6.0 dB = half volume !
