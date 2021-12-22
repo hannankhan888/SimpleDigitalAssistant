6e5e2c535ad26a675bfcb095f3eb07c057e2e6c5
hannankhan888 <hannankhan888@gmail.com>
2021-12-22 09:21:47 -0600

    added license function to app.py

--- app.py
+++ app.py
@@ -227,8 +227,32 @@ class RootWindow(QMainWindow):
         pass
 
     def license(self):
+        license_str = """
+MIT License
 
-        pass
+Copyright (c) 2021 Hannan, Salman, Reza, Ali
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE."""
+        license_dialog = FramelessDialog(self, license_str, self.normal_bg, self.minimize_button_label_highlight_bg,
+                                         self.normal_color, self.highlight_color, self.close_button_label_highlight_bg,
+                                         self.close_button_label_highlight_color, "License", QFont(self.lato_font_family, 12))
+        license_dialog.exec_()
 
     def minimize_app(self):
         self.showMinimized()
