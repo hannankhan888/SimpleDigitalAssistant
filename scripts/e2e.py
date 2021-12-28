033ded5880a10b00d64f312ff0bf0906be8d0b57
Ali Abdul-Hameed <ali1.abdulhameed@gmail.com>
2021-12-27 19:14:39 -0600

    Fixed e2e.py for windows. Works with Watson.

--- scripts/e2e.py
+++ scripts/e2e.py
@@ -1,10 +1,10 @@
 import sys
 # insert at 1, 0 is the script path (or '' in REPL)
-sys.path.insert(1, 'VoiceRecognition/nlu')
-import Watson
+sys.path.insert(1, 'C:\\Users\\Ali Abdul-Hameed\\PycharmProjects\\SimpleDigitalAssistant\\VoiceRecognition\\nlu')
+from Watson import Watson
 
-nlu = Watson.Watson()
+nlu = Watson()
 
-response = nlu.send_message("What is five time five times four")
+response = nlu.send_message("Show me the weather in India")
 print(nlu.get_intents(response))
 
