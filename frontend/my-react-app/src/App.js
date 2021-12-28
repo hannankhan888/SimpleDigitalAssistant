aaca412f105b578415a851ab3794dc8f49d6b0cf
Ali Abdul-Hameed <ali1.abdulhameed@gmail.com>
2021-12-28 00:09:28 -0600

    Fixed e2e.py for windows. Works with Watson.

---
+++ frontend/my-react-app/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+    <div className="box">
+      <h2>Max</h2>
+      <button> Start/Stop </button>
+    </div>
+    <div className="box">
+      <h2>Output</h2>
+    </div>
+
+    </div>
+  );
+}
+
+export default App;
