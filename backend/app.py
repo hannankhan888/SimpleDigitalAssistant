2b4af291dddf2a32a95d4eebe298ef8a21a89d5a
Reza Mohideen <reza.mohideen@ibm.com>
2021-12-29 16:11:59 -0600

    websocket streaming from mic

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,9 @@
 from fastapi import FastAPI, WebSocket
 import random
+import json
+from scipy.io.wavfile import write
+import numpy as np
+
 
 app = FastAPI()
 
@@ -15,10 +19,14 @@ async def websocket_endpoint(websocket: WebSocket):
     while True:
         try:
             # Wait for any message from the client
-            data = await websocket.receive_text()
-            print(data)
+            data = await websocket.receive()
+            print(len(data['bytes']), type(data['bytes']))
+            float64_buffer = np.frombuffer(data['bytes'], dtype=np.int16) / 32000
+            write('test5.wav', 16000, float64_buffer)
             # Send message to the client
-            resp = {'value': 1}
+            val = random.randint(1,5)
+            print(val)
+            resp = {'value': val}
             await websocket.send_json(resp)
         except Exception as e:
             print('error:', e)
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,44 +1,73 @@
 import logo from './logo.svg';
 import './App.css';
-import React from 'react';
+import React, {useState, useRef, useEffect} from 'react';
 
-class App extends React.Component {
-  componentDidMount() {
-    const ws = new WebSocket('ws://localhost:8000/ws')
-    ws.onmessage = this.onMessage
+function App() {
+  const [isRecording, setRecording] = useState(false);
+  const ws = useRef(null);
+  const mediaRecorder = useRef(null);
+  let chunks = useRef([]);
 
-    this.setState({
-      ws: ws,
-      // Create an interval to send echo messages to the server
-      interval: setInterval(() => ws.send('echo'), 1000)
+  useEffect(() => {
+    navigator.mediaDevices.getUserMedia({video: false, audio: true}).then( stream => {
+      mediaRecorder.current = new MediaRecorder(stream);
+      mediaRecorder.current.ondataavailable = e => {
+        if (e.data) {
+          e.data.arrayBuffer().then(buffer => {
+            console.log(e.data);
+            ws.current.send(e.data);
+          });
+          chunks.current.push(e.data);
+          
+          // ws.current.send(e.data.text());
+          // console.log('sending data', e.data.text());
+        }
+      }
     })
-  }
-
-  componentWillUnmount() {
-    const {ws, interval} = this.state;
-    ws.close()
-    clearInterval(interval)
-  }
-
-  onMessage = (ev) => {
     
-    console.log(JSON.parse(ev.data))
-  }
+    
+    ws.current = new WebSocket('ws://localhost:8000/ws');
+    ws.current.onopen = () => {
+      console.log('ws opened')
+    };
+    ws.current.onmessage = e => {
+      const message = JSON.parse(e.data);
+      console.log('e', message.value);
+    };
+    ws.current.onclose = () => console.log('ws closed');
 
-  render() {
-    return (
-      <div className="container">
-    <div className="box">
-      <h2>Max</h2>
-      <button> Start/Stop </button>
-    </div>
-    <div className="box">
-      <h2>Output</h2>
-    </div>
+    return () => {
+        ws.current.close();
+    };
+  }, []);
 
+  useEffect(() => {
+      if (!isRecording) {
+        if (mediaRecorder.current !== null) mediaRecorder.current.stop();
+        return
+
+      };
+      if (isRecording && ws.current.readyState === 1) {
+        mediaRecorder.current.start(5000);
+        console.log('starting recording')
+      };
+
+
+  }, [isRecording]);
+
+  return (
+    <div className='container'>
+      <div className='box'>
+        <h2>Max</h2>
+        <button onClick={() => setRecording(!isRecording)}>
+                {isRecording ? "Stop" : "Start"}
+        </button>
+      </div>
+      <div className='box'>
+        <h2>Output</h2>
+      </div>
     </div>
     )
   }
-}
 
 export default App;
