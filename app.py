3e1fbb9f22bb8460f894f88aae9005a3c7f51175
hannankhan888 <hannankhan888@gmail.com>
2021-12-29 07:05:40 -0600

    renamed threads function, and updated function call to del instead of pop

--- app.py
+++ app.py
@@ -330,7 +330,7 @@ class RootWindow(QMainWindow):
         There is an option to play the recorded voice back out loud to you (just in case
         for debugging). Simply uncomment the appropriate line in self.get_voice_command."""
 
-        self._flush_threads()
+        self._update_threads()
         self.mic_label.invert_active_state()
         self.recording_thread = threading.Thread(target=self.get_voice_command)
         self.recording_thread.setDaemon(True)
@@ -416,11 +416,11 @@ class RootWindow(QMainWindow):
             self.output_label.append("Please try again.")
         self.output_label.moveCursor(QtGui.QTextCursor.End)
 
-    def _flush_threads(self):
+    def _update_threads(self):
         for idx, thread in enumerate(self.threads):
             if not thread.is_alive():
                 thread.join()
-                self.threads.pop(idx)
+                del self.threads[idx]
 
     def about(self) -> None:
         """This function takes care of the about dialog."""
