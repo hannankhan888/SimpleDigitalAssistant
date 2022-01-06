02f710d5c5e19c95d07d8b41ab63241b4f681f7b
Ali Abdul-Hameed <ali1.abdulhameed@gmail.com>
2022-01-05 22:43:09 -0600

    Fixed stocks.py.

--- Actions/stocks.py
+++ Actions/stocks.py
@@ -13,17 +13,24 @@ import csv
 
 import yfinance as yf
 
+def get_symbol(user_stock_inquiry: str) -> None:
 
-def get_symbol(user_stock_inquiry: str) -> str:
-    with open('resources/symbols.csv', mode='r') as inp:
+    with open('../resources/symbols.csv', mode='r') as inp:
         reader = csv.reader(inp)
         stock_dict = {rows[0]: rows[1] for rows in reader}
-    stock_lower_dict = {k.lower(): v for (k, v) in stock_dict.items()}
+    stock_lower_dict = {k.lower(): v for(k, v) in stock_dict.items()}
+
+    extra_words = ['co.', 'corp.', 'communications', 'inc.', 'laboratories', 'ltd.', 'plc.', 'of', 'the']
 
     for key in stock_lower_dict:
         try:
-            ending = key.split()
-            if ending[0] in user_stock_inquiry.lower() or ending[0] and ending[1] in user_stock_inquiry.lower():
+            company_names = key.split(',')
+            company_names[0] = key.replace('&', ' and ')
+            # print(company_names[0])
+            for word in extra_words:
+                if word in company_names[0]:
+                    company_names[0] = company_names[0].replace(' ' + word, '')
+            if company_names[0] in user_stock_inquiry.lower():
                 return stock_lower_dict.get(key)
         except IndexError:
             continue
@@ -32,33 +39,30 @@ def get_symbol(user_stock_inquiry: str) -> str:
 
 
 def company_stock(user_inquiry):
-    stocks_str = ""
     ticker = get_symbol(user_inquiry.lower())
-    if ticker is not None:
+    if ticker != None:
         company = yf.Ticker(ticker)
 
         try:
-            com_stock = {'name': company.info['shortName'],
-                         'price': company.info['currentPrice'],
-                         'recommendation': company.info['recommendationKey']}
-            stocks_str = f"{company.info['shortName']} Current Price is {company.info['currentPrice']}."
-            stocks_str += f" The recommendation is to {company.info['recommendationKey']}."
+            company_stock = {'name': company.info['shortName'],
+                             'price': company.info['currentPrice'],
+                             'recommendation': company.info['recommendationKey']}
+
             print(company.info['shortName'] + " (" + company.info['symbol'] + ")")
             print("Current Price: ", company.info['currentPrice'])
             print("Recommendation: ", company.info['recommendationKey'])
-            return stocks_str
+            return company_stock
         except KeyError:
-            return "I could not find the company symbol."
+            print("Company cannot be found.")
 
     else:
-        return "I could not find the company symbol."
+        print("The company symbol cannot be found.")
+        return None
 
 
 if __name__ == "__main__":
     company_stock("tell me about abbvie")  # stock that exists (works)
-    company_stock("show me abbott stocks")
-    company_stock("give me bob stock")  # stock that does not exist
-    company_stock("show me GOOGLE stock")  # stock that exists but all caps
-    company_stock("tell me the stock price of facebook")  # stock for a company with quotations around it
-    # TODO: run through these new test cases i made up after testing via max:
-    company_stock("what's the price of golestock")  # should not return a stock at all.
+    company_stock("show me abbott stocks")  # stock that exists (works)
+    company_stock("give me bob stock")  # stock that does not exist (works)
+    company_stock("show me GOOGLE stock")  # stock that exists but all caps (works)
+    company_stock("tell me the stock price of facebook")  # stock for a company (works)
